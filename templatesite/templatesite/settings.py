bd758b9b73969efd97188a87f3968ab85061af49
Shirguin <shirguin@yandex.ru>
2022-06-20 07:13:21 +0300

    Auth

--- templatesite/templatesite/settings.py
+++ templatesite/templatesite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'main.apps.MainConfig',
+    'authapp.apps.AuthappConfig',
 ]
 
 MIDDLEWARE = [
@@ -123,3 +124,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'authapp.SiteUser'
